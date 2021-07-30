bcb0c4b304f3896929715033db49bf99a623d34d
Chris Nesbitt-Smith <chris@cns.me.uk>
2021-07-30 17:26:46 +0100

    more work on gui

---
+++ gui/form.js
@@ -0,0 +1,235 @@
+var editor = new JSONEditor(document.getElementById('editor_holder'), {
+  // ajax: true,
+  theme: 'bootstrap4',
+  disable_edit_json: true,
+  disable_collapse: true,
+  disable_properties: true,
+  schema: {
+    type: "object",
+    properties: {
+      "Who are you": {
+        type: "object",
+        required: [
+          "Name",
+          "Company",
+          "Email Address",
+          "Github Username",
+        ],
+        properties: {
+          Name: {
+            type: "string",
+            default: "John Doe"
+          },
+          Company: {
+            type: "string",
+            default: "Acme inc"
+          },
+          "Email Address": {
+            type: "string",
+            default: "user@acme.inc"
+          },
+          "Github Username": {
+            type: "string",
+            default: "johndoeacme"
+          },
+        }
+      },
+      "Nodes": {
+        type: "object",
+        properties: {
+          "Node Type": {
+            enum: [
+              "t2.medium",
+              "m5.4xlarge",
+              "m5.8xlarge",
+              "m5.16xlarge"
+            ],
+            type: "string"
+          },
+          "Node minimum count": {
+            type: "integer",
+            maximum: 1000,
+            minimum: 1,
+            default: 70
+          },
+          nodeScaling: {
+            type: "checkbox",
+            title: "Node auto scaling (+$300)"
+          },
+          "Node maximum count": {
+            type: "integer",
+            maximum: 1000,
+            minimum: 1,
+            default: 1000,
+            options: {
+              dependencies: {
+                "nodeScaling": true,
+              }
+            }
+          },
+        },
+      },
+      "Frontend": {
+        type: "object",
+        properties: {
+          "Minimum Replica": {
+            type: "integer",
+            minimum: 1,
+            default: 200
+          },
+          "CPU Limit": {
+            type: "integer",
+            minimum: 50,
+            default: 800
+          },
+          "Memory Limit": {
+            type: "integer",
+            minimum: 32,
+            default: 256
+          },
+          HPA: {
+            type: "checkbox",
+            title: "Frontend autoscaling (Horizontal Pod Autoscaler) (+$200)",
+          },
+          "Maximum Replica": {
+            type: "integer",
+            minimum: 1,
+            default: 2000,
+            options: {
+              dependencies: {
+                "HPA": true,
+              }
+            }
+          },
+          "CPU Scaling Threshold": {
+            type: "integer",
+            maximum: 100,
+            minimum: 1,
+            default: 70,
+            options: {
+              dependencies: {
+                "HPA": true,
+              }
+            }
+          },
+        }
+      },
+
+      "Backend": {
+        type: "object",
+        properties: {
+          "Minimum Replica": {
+            type: "integer",
+            minimum: 1,
+            default: 200
+          },
+          "CPU Limit": {
+            type: "integer",
+            minimum: 250,
+            default: 850
+          },
+          "Memory Limit": {
+            type: "integer",
+            minimum: 512,
+            default: 1024
+          },
+          HPA: {
+            type: "checkbox",
+            title: "Backend autoscaling (Horizontal Pod Autoscaler) (+$200)",
+          },
+          "Maximum Replica": {
+            type: "integer",
+            minimum: 1,
+            default: 800,
+            options: {
+              dependencies: {
+                "HPA": true,
+              }
+            }
+          },
+          "CPU Scaling Threshold": {
+            type: "integer",
+            maximum: 100,
+            minimum: 1,
+            default: 70,
+            options: {
+              dependencies: {
+                "HPA": true,
+              }
+            }
+          },
+        }
+      },
+      "Database": {
+        type: "object",
+        properties: {
+          "Minimum Replica": {
+            type: "integer",
+            minimum: 1,
+            default: 10
+          },
+          "CPU Limit": {
+            type: "integer",
+            minimum: 800,
+            default: 15000
+          },
+          "Memory Limit": {
+            type: "integer",
+            minimum: 1024,
+            default: 4096
+          },
+          HPA: {
+            type: "checkbox",
+            title: "Database autoscaling (Horizontal Pod Autoscaler) (+$1500)",
+          },
+          "Maximum Replica": {
+            type: "integer",
+            minimum: 1,
+            default: 70,
+            options: {
+              dependencies: {
+                "HPA": true,
+              }
+            }
+          },
+          "CPU Scaling Threshold": {
+            type: "integer",
+            maximum: 100,
+            minimum: 1,
+            default: 50,
+            options: {
+              dependencies: {
+                "HPA": true,
+              }
+            }
+          },
+        }
+      },
+    }
+  }
+})
+
+
+// Hook up the submit button to log to the console
+document.getElementById('submit').addEventListener('click', function () {
+  // Get the value from the editor
+  console.log(editor.getValue())
+})
+
+editor.on('change', function () {
+  // Get an array of errors from the validator
+  var errors = editor.validate()
+
+  var indicator = document.getElementById('submit')
+
+  if (errors.length)
+    indicator.style.color = 'red'
+  else
+    indicator.style.color = 'green'
+
+})
+// $("button").click(() => {
+//   $("#result").toggle()
+//   $(".container").toggle()
+
+// })
---
+++ gui/gui.js
@@ -0,0 +1,124 @@
+// set the dimensions and margins of the graph
+const margin = { top: 15, left: 60 }
+
+const loadTime = 3
+d3.json('./output.json').then(data => {
+
+  const totalRequests = Math.ceil(data.reduce((accumulator, interval) => accumulator + interval.requests, 0))
+  const failedRequests = Math.ceil(data.reduce((accumulator, interval) => accumulator + interval.failedRequests, 0))
+  const spend = data.reduce((accumulator, interval) => accumulator + interval.cost, 0)
+  const penalties = failedRequests * 0.01
+  const savings = 10000 - spend
+
+  countup("#totalrequests", totalRequests)
+  countup("#failedrequests", failedRequests)
+  countup("#spend", spend)
+  countup("#savings", savings)
+  countup("#penalties", penalties)
+  countup("#totalscore", savings - penalties)
+
+  function countup(selector, value) {
+    $(selector).prop('Counter', 0).animate({
+      Counter: value
+    }, {
+      duration: loadTime * 1000,
+      easing: 'swing',
+      step: function (now) {
+        $(selector).text(this.Counter.toFixed(0));
+      }
+    })
+  }
+
+  const graphs = [
+    {
+      id: "total_requests_vs_failed",
+      keys: ["requests", "failedRequests"],
+    }
+  ]
+
+  graphs.forEach(graph => {
+    const height = 385
+    const div = document.createElement("div")
+    div.id = graph.id
+    document.getElementById("my_dataviz").appendChild(div)
+
+    const svg = d3
+      .select(`#${graph.id}`)
+      .append('svg')
+      .attr("preserveAspectRatio", "xMinYMin meet")
+      .attr("viewBox", "0 0 960 500")
+      .append('g')
+      .attr('transform', `translate(${margin.left},${margin.top})`)
+
+    const color = d3.scaleOrdinal()
+      .domain(graph.keys)
+      .range(d3.schemeSet2);
+
+    const x = d3
+      .scaleTime()
+      .domain(d3.extent(data, d => d.time))
+      .range([0, 900])
+
+    svg
+      .append('g')
+      .attr('transform', `translate(0, ${height})`)
+      .call(d3.axisBottom(x))
+      .selectAll("text")
+      .attr("transform", "translate(-10,10)rotate(-45)")
+
+    const y = d3
+      .scaleLinear()
+      .domain([0, d3.max(data, d => d.requests)])
+      .range([height, 0])
+
+    svg.append('g').call(d3.axisLeft(y))
+
+    graph.keys.forEach(key => {
+      svg
+        .append('path')
+        .datum(data)
+        .attr("fill", "none")
+        .attr("stroke", (d) => color(key))
+        .attr('stroke-width', 1.5)
+        .attr(
+          'd',
+          d3
+            .line()
+            .x(d => x(d.time))
+            .y(d => y(d[key]))
+        )
+        .transition()
+        .duration(5 * 1000)
+        .attrTween("stroke-dasharray", tweenDash)
+
+      function tweenDash() {
+        var l = this.getTotalLength(),
+          i = d3.interpolateString("0," + l, l + "," + l)
+        return t => i(t)
+      }
+    })
+
+    var legendSizeSquare = 30
+    svg.selectAll("mydots")
+      .data(graph.keys)
+      .enter()
+      .append("rect")
+      .attr("x", 100)
+      .attr("y", function (d, i) { return 100 + i * (legendSizeSquare + 5) }) // 100 is where the first dot appears. 25 is the distance between dots
+      .attr("width", legendSizeSquare)
+      .attr("height", legendSizeSquare)
+      .style("fill", function (d) { return color(d) })
+
+    svg.selectAll("mylabels")
+      .data(graph.keys)
+      .style("fill", function (d) { return "white" })
+      .enter()
+      .append("text")
+      .attr("x", 100 + legendSizeSquare * 1.2)
+      .attr("y", function (d, i) { return 100 + i * (legendSizeSquare + 5) + (legendSizeSquare / 2) }) // 100 is where the first dot appears. 25 is the distance between dots
+      .style("fill", function (d) { return color(d) })
+      .text(function (d) { return d })
+      .attr("text-anchor", "left")
+      .style("alignment-baseline", "middle")
+  })
+})
