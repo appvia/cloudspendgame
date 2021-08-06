a3ea73304b565e4a61e84a78b88aa56df473d061
Chris Nesbitt-Smith <chris@cns.me.uk>
2021-08-06 14:49:13 +0100

    start of a local server

--- local.ts
+++ local.ts
@@ -1,6 +1,33 @@
+// @ts-nocheck
 import * as app from './'
 
 // console.log(app.play({}, {}))
 // const mod = require('./')
 
 // mod.pageHandler({}, {}, console.log)
+const http = require('http')
+
+const host = 'localhost'
+const port = 8000
+
+const requestListener = async (req, res) => {
+  // console.log(req)
+  res.writeHead(200)
+  if (req.method === 'GET') {
+    const lambdaEvent = {
+      rawPath: req.url
+    }
+    const response = await app.pageHandler(lambdaEvent)
+    console.log(lambdaEvent)
+    res.end(response.body)
+    // return
+  } else if (req.method === 'POST') {
+    // game api
+  }
+  // res.end('My first server!')
+}
+
+const server = http.createServer(requestListener)
+server.listen(port, host, () => {
+  console.log(`Server is running on http://${host}:${port}`)
+})
