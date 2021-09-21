50d86a121993e1295a6a1824dab18ac75e601e5c
Chris Nesbitt-Smith <chris@cns.me.uk>
2021-09-21 15:56:29 +0100

    refresh scores every 30secs

--- docs/scoreboard.js
+++ docs/scoreboard.js
@@ -1,11 +1,17 @@
-$.get({
-  url: 'https://blackfriday.sa-team.teams.kore.appvia.io/scoreboard',
-  contentType: 'application/json',
-}).then(response => {
-  const table = document.getElementById("scoretable")
-  response.map(score => {
-    const row = table.insertRow()
-    row.insertCell().innerHTML = filterXSS(score.handle)
-    row.insertCell().innerHTML = Math.ceil(score.score)
+const scoreboard = () =>
+  $.get({
+    url: 'https://blackfriday.sa-team.teams.kore.appvia.io/scoreboard',
+    contentType: 'application/json',
+  }).then(response => {
+    $("#scoretable tr").not(":eq(0)").remove()
+    const table = document.getElementById("scoretable")
+    response.map(score => {
+      const row = table.insertRow()
+      row.insertCell().innerHTML = filterXSS(score.handle)
+      row.insertCell().innerHTML = Math.ceil(score.score)
+    })
   })
-})
+
+
+scoreboard()
+setInterval(scoreboard, 30_000)
